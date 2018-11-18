bc5d79525049ca43b6632c1307638d26681cc10f
James Neill <j.neill.inbox@gmail.com>
2018-11-18 02:14:04 -0500

    create results array instead of print to console

--- components/ai.py
+++ components/ai.py
@@ -1,5 +1,6 @@
 class BasicMonster:
     def take_turn(self, target, game_map, entities):
+        results = []
         monster = self.owner
 
         if game_map.fov[monster.x, monster.y]:
@@ -7,4 +8,7 @@ class BasicMonster:
                 monster.move_towards(target.x, target.y, game_map, entities)
 
             elif target.fighter.hp > 0:
-                monster.fighter.attack(target)
+                attack_results = monster.fighter.attack(target)
+                results.extend(attack_results)
+
+        return results
